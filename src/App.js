3c106436ee34bbd9e686f03d3c861b31b426fd3f
sharma-sagar2002 <sagar2610sharma@gmail.com>
2024-07-22 16:02:02 +0530

    project uploaded

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import { useForm } from 'react-hook-form';
+import './App.css';
+import Form from './Components/Form';
+import CustomHookForm from './Components/SimpleForm';
+
+function App() {
+  return (
+    // <CustomHookForm/>
+   <Form/>
+)
+}
+
+export default App;
